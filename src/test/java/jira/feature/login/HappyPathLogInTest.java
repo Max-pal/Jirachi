4973815adf3e1400160c820788ea34094b90ddbb
Max <max.pal@outlook.com>
2020-03-31 22:52:52 +0200

    Add unautherized login attempt test

--- src/test/java/jira/feature/login/HappyPathLogInTest.java
+++ src/test/java/jira/feature/login/HappyPathLogInTest.java
@@ -12,7 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ValidLogInTest {
+public class HappyPathLogInTest {
     WebDriver driver;
     String baseURL;
     String USERNAME = System.getenv("USERNAME");
@@ -20,16 +20,18 @@ public class ValidLogInTest {
     WebElement usernameField;
     WebElement passwordField;
     WebElement loginButton;
+    WebDriverWait wait;
 
     @BeforeEach
     public void setUp() {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Test
-    public void happyPathMainPage() {
+    public void LogInMainPage() {
         baseURL = "https://jira.codecool.codecanvas.hu/";
         driver.get(baseURL);
         usernameField = driver.findElement(By.xpath("//*[@id=\"login-form-username\"]"));
@@ -41,7 +43,7 @@ public class ValidLogInTest {
     }
 
     @Test
-    public void happyPathLoginPage() {
+    public void LogInLoginPage() {
         baseURL = "https://jira.codecool.codecanvas.hu/login.jsp";
         driver.get(baseURL);
         usernameField = driver.findElement(By.xpath("//*[@id=\"login-form-username\"]"));
@@ -50,11 +52,11 @@ public class ValidLogInTest {
         usernameField.sendKeys(USERNAME);
         passwordField.sendKeys(PASSWORD);
         loginButton.click();
+
     }
 
     @AfterEach
     public void tearDown() {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header-details-user-fullname\"]/span/span/img")));
         WebElement profilepic = driver.findElement(By.xpath("//*[@id=\"header-details-user-fullname\"]/span/span/img"));
         String check = profilepic.getAttribute("alt");
